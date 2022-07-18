81aeba9933badccc7731ed4f665f3c2f6592f4b1
andrey-targa <andrey.targaprado@gmail.com>
2022-07-18 10:24:00 -0300

    segundo commit

--- src/App.java
+++ src/App.java
@@ -1,13 +1,7 @@
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 public class App {
